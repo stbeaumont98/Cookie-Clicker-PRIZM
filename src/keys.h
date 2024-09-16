2a1efc1973bdf0b69892951615950f979398a6f9
Spencer Beaumont <spence.beaumont@gmail.com>
2024-09-16 03:36:07 -0400

    Key handling code moved to separate files.

---
+++ src/keys.h
@@ -0,0 +1,13 @@
+#ifndef KEYS_H_
+#define KEYS_H_
+
+int PRGM_GetKey();
+
+void keyupdate(void);
+
+int keydownlast(int basic_keycode);
+int keydownhold(int basic_keycode);
+
+int keyPressed(int basic_keycode);
+
+#endif
