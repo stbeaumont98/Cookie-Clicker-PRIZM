d768f04c90b6e775f90dfe7a097412a5b9947865
Spencer Beaumont <spence.beaumont@gmail.com>
2024-09-17 18:22:05 -0400

    Updated abbreviations, added radix parameter to itoa.

--- src/convert.c
+++ src/convert.c
@@ -16,64 +16,64 @@ char *get_display_val(double val, bool disp_dec, bool abrev) {
 		disp_val = val;
 	} else if (val >= 1E6 && val < 1E9) {
 		disp_val = val * 1E-6;
-		strcpy(suffix, abrev ? " M" : " million");
+		strcpy(suffix, abrev ? " mil." : " million");
 	} else if (val >= 1E9 && val < 1E12) {
 		disp_val = val * 1E-9;
-		strcpy(suffix, abrev ? " B" : " billion");
+		strcpy(suffix, abrev ? " bil." : " billion");
 	} else if (val >= 1E12 && val < 1E15) {
 		disp_val = val * 1E-12;
-		strcpy(suffix, abrev ? " T" : " trillion");
+		strcpy(suffix, abrev ? " tril." : " trillion");
 	} else if (val >= 1E15 && val < 1E18) {
 		disp_val = val * 1E-15;
-		strcpy(suffix, abrev ? " Qa" : " quadrillion");
+		strcpy(suffix, abrev ? " quad." : " quadrillion");
 	} else if (val >= 1E18 && val < 1E21) {
 		disp_val = val * 1E-18;
-		strcpy(suffix, abrev ? " Qi" : " quintillion");
+		strcpy(suffix, abrev ? " quin." : " quintillion");
 	} else if (val >= 1E21 && val < 1E24) {
 		disp_val = val * 1E-21;
-		strcpy(suffix, abrev ? " Sx" : " sextillion");
+		strcpy(suffix, abrev ? " sext." : " sextillion");
 	} else if (val >= 1E24 && val < 1E27) {
 		disp_val = val * 1E-24;
-		strcpy(suffix, abrev ? " Sp" : " septillion");
+		strcpy(suffix, abrev ? " sept." : " septillion");
 	} else if (val >= 1E27 && val < 1E30) {
 		disp_val = val * 1E-27;
-		strcpy(suffix, abrev ? " Oc" : " octillion");
+		strcpy(suffix, abrev ? " oct." : " octillion");
 	} else if (val >= 1E30 && val < 1E33) {
 		disp_val = val * 1E-30;
-		strcpy(suffix, abrev ? " No" : " nonillion");
+		strcpy(suffix, abrev ? " non." : " nonillion");
 	} else if (val >= 1E33 && val < 1E36) {
 		disp_val = val * 1E-33;
-		strcpy(suffix, abrev ? " Dc" : " decillion");
+		strcpy(suffix, abrev ? " dec." : " decillion");
 	} else if (val >= 1E36 && val < 1E39) {
 		disp_val = val * 1E-36;
-		strcpy(suffix, abrev ? " Udc" : " undecillion");
+		strcpy(suffix, abrev ? " udc." : " undecillion");
 	} else if (val >= 1E39 && val < 1E42) {
 		disp_val = val * 1E-39;
-		strcpy(suffix, abrev ? " Ddc" : " duodecillion");
+		strcpy(suffix, abrev ? " ddc." : " duodecillion");
 	} else if (val >= 1E42 && val < 1E45) {
 		disp_val = val * 1E-42;
-		strcpy(suffix, abrev ? " Tdc" : " tredecillion");
+		strcpy(suffix, abrev ? " tdc." : " tredecillion");
 	} else if (val >= 1E45 && val < 1E48) {
 		disp_val = val * 1E-45;
-		strcpy(suffix, abrev ? " Qadc" : " quattuordecillion");
+		strcpy(suffix, abrev ? " qadc." : " quattuordecillion");
 	} else if (val >= 1E48 && val < 1E51) {
 		disp_val = val * 1E-48;
-		strcpy(suffix, abrev ? " Qidc" : " quindecillion");
+		strcpy(suffix, abrev ? " qidc." : " quindecillion");
 	} else if (val >= 1E51 && val < 1E54) {
 		disp_val = val * 1E-51;
-		strcpy(suffix, abrev ? " Sxdc" : " sexdecillion");
+		strcpy(suffix, abrev ? " sxdc." : " sexdecillion");
 	} else if (val >= 1E54 && val < 1E57) {
 		disp_val = val * 1E-54;
-		strcpy(suffix, abrev ? " Spdc" : " septendecillion");
+		strcpy(suffix, abrev ? " spdc." : " septendecillion");
 	} else if (val >= 1E57 && val < 1E60) {
 		disp_val = val * 1E-57;
-		strcpy(suffix, abrev ? " Ocdc" : " octodecillion");
+		strcpy(suffix, abrev ? " ocdc." : " octodecillion");
 	} else if (val >= 1E60 && val < 1E63) {
 		disp_val = val * 1E-60;
-		strcpy(suffix, abrev ? " Nodc" : " novemdecillion");
+		strcpy(suffix, abrev ? " nodc." : " novemdecillion");
 	} else if (val >= 1E63 && val < 1E66) {
 		disp_val = val * 1E-63;
-		strcpy(suffix, abrev ? " Vg" : " vigintillion");
+		strcpy(suffix, abrev ? " vg." : " vigintillion");
 	}
 
 	if (val < 1E6 && disp_dec)
@@ -105,7 +105,7 @@ char *disp_decimal(double val, int32_t dec_pnt) {
 		strcpy(buffer, tmp);
 		free(tmp);
 	} else
-		itoa(integer, buffer);
+		itoa(integer, buffer, 10);
 	
 	if (dec_pnt != 0) {
 		int32_t decimal = round2((val - integer) * dec_pnt);
@@ -113,13 +113,13 @@ char *disp_decimal(double val, int32_t dec_pnt) {
 		if (decimal != 0) {
 
 			char tmp[9];
-			itoa(decimal, tmp);
+			itoa(decimal, tmp, 10);
 			int dec_len = strlen(tmp);
 
 			while (decimal % 10 == 0)
 				decimal /= 10;
 				
-			itoa(decimal, tmp);
+			itoa(decimal, tmp, 10);
 			
 			strcat(buffer, ".");
 
@@ -143,20 +143,20 @@ char *disp_comma(int32_t val) {
 	char *buffer = malloc(10);
 
 	if (val < 1000) {
-		itoa(val, buffer);
+		itoa(val, buffer, 10);
 		return buffer;
 	}
 
 	int16_t front = (int16_t) floor2((double) val / 1000.);
 	int16_t back = round2(val - (front * 1000));
 
-	itoa(front, buffer);
+	itoa(front, buffer, 10);
 
 	strcat(buffer, ",");
 
 	char *back_buf = malloc(5);
 
-	itoa(back, back_buf);
+	itoa(back, back_buf, 10);
 
 	for (int i = 0; i < 3 - strlen(back_buf); i++)
 		strcat(buffer, "0");
