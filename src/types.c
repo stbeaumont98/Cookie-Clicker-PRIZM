4e702d4835bb176a9fbcdcec670752b77b80adb3
Spencer Beaumont <spence.beaumont@gmail.com>
2024-09-19 01:03:28 -0400

    Misc bug fix

--- src/types.c
+++ src/types.c
@@ -8,7 +8,7 @@ void reset_buildings(struct CookieData *data) {
 	for(int i = 0; i < 20; i++) {
 		data->buildings[i].price = base_prices[i];
 		data->buildings[i].owned = 0;
-		data->buildings[i].locked = !(strcmp(building_types[i], "Cursor") == 0 || strcmp(building_types[i], "Grandm*a") == 0);
+		data->buildings[i].locked = !(strcmp(building_types[i], "Cursor") == 0 || strcmp(building_types[i], "Grandma") == 0);
 		data->buildings[i].hidden = true;
 	}
 }
